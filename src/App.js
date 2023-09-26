ab920bf3833407328e25ac8abd589c027d7d23a3
Aghasy8888 <aghasghazaryan8888@gmail.com>
2023-09-26 21:09:37 +0400

    website front-end done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { MusicWebsite } from "./components";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MusicWebsite />} />
-      </Routes>
-    </BrowserRouter>
+    <MusicWebsite />
   );
 }
 
